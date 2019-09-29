702bdd208d4e5a6b7cb4a77ca31f540469edc89a
iamkenos <iamkenos@users.noreply.github.com>
2019-09-29 15:53:09 +0800

    File upload and frame switch (#4)
    * file upload
    * iframe switch

---
+++ demo/test/pages/meta/demo.meta.js
@@ -0,0 +1,26 @@
+export default {
+  default: {
+    url: '/',
+    title: 'DEMO APP',
+    locators: {
+      // Section 11
+      'Section 11': '//h3[text()="SECTION 11"]',
+      'Same window link': '//span[./a[@id="linkSameWindow"]]/a',
+
+      // Section 13
+      'Section 13': '//h3[text()="SECTION 13"]',
+      'Test input': '#testInput',
+      'Upload input': '#uploadInput',
+
+      // Section 15
+      'Section 15': '//h3[text()="SECTION 15"]',
+      'Create button': '#waitForCreateBtn',
+
+      // Section 16
+      'Section 16': '//h3[text()="SECTION 16"]',
+      'Key bind': '#testKeyResponse',
+      'Sample Iframe': '//iframe',
+      'Iframe input': '#iframeInput'
+    }
+  }
+};

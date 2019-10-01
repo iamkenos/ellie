7f4e3164c0b8785f652970103c3696d745e18db6
iamkenos <alexis.matunog@gmail.com>
2019-10-02 03:13:03 +0800

    refactor: browserstack service and added browserstack local

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -1,13 +1,13 @@
 import * as yargs from "yargs";
 
-import { OVERRIDE_OPTS, USAGE } from "./config";
+import { CONFIG_OPTIONS, USAGE } from "./config";
 import runCore from "../core/run";
 import runCLI from "./run";
 
 export async function start(): Promise<any> {
   try {
     let args = yargs.usage(USAGE.trim()).wrap(120);
-    for (const opt of OVERRIDE_OPTS) {
+    for (const opt of CONFIG_OPTIONS) {
       args = args.option(opt.name, opt as any);
     }
 

fa390f51c54f0be711c0e1c14dd9efd45f0b1fc3
iamkenos <alexis.matunog@gmail.com>
2020-02-03 19:28:11 +0800

    style: removed markers on step defs

--- src/core/steps/definitions/action.ts
+++ src/core/steps/definitions/action.ts
@@ -33,139 +33,116 @@ import windowSizeSet from "../glue/action/windowSizeSet";
 import windowSwitchFrame from "../glue/action/windowSwitchFrame";
 import windowUrlSet from "../glue/action/windowUrlSet";
 
-// ok
 When(
   /^I (click|double click|script click) the (link|button|element) "([^"]*)?"(?: again)?$/,
   elemClick
 );
 
-// ok
 When(
   /^I drag the element "([^"]*)?" to the element "([^"]*)?"$/,
   elemDragAndDrop
 );
 
-// ok
 When(
   /^I upload the file "([^"]*)?" to the (?:field|element) "([^"]*)?"$/,
   elemFileUpload
 );
 
-// ok
 When(
   /^I focus on the (?:field|element) "([^"]*)?"$/,
   elemFocus
 );
 
-// ok
 When(
   /^I move to the element "([^"]*)?"(?: with an offset of (\d+),(\d+))?$/,
   elemMoveTo
 );
 
-// ok
 When(
   /^I select the option with (index|label|[^" ]*) "([^"]*)?" from the dropdown "([^"]*)?"$/,
   elemOptionSelect
 );
 
-// ok
 When(
   /^I (select|deselect) the (?:element|option|check box|toggle item|radio button) "([^"]*)?"$/,
   elemSelect
 );
 
-// ok
 When(
   /^I scroll to the element "([^"]*)?"$/,
   elemScrollTo
 );
 
-// ok
 When(
   /^I clear the (?:field|element) "([^"]*)?"$/,
   elemValueClear
 );
 
-// ok
 When(
   /^I (type|append) "([^"]*)?" on the (?:field|element) "([^"]*)?"$/,
   elemValueSet
 );
 
-// ok
 When(
   /^I (type|append) a multi-line value on the (?:field|element) "([^"]*)?":$/,
   elemValueSetMultiLine
 );
 
-// ok
 When(
   /^I start to intercept (?:ajax|xhr) requests$/,
   windowAjaxIntercept
 );
 
-// ok
 When(
   /^I close all except the parent (?:window|tab)$/,
   windowChildrenClose
 );
 
-// ok
 When(
   /^I delete the cookie "([^"]*)?"$/,
   windowCookieDelete
 );
 
-// ok
 When(
   /^I delete the cookies$/,
   windowCookiesDelete
 );
 
-// ok
 When(
   /^I set the cookie "([^"]*)?" with the content "([^"]*)?"$/,
   windowCookieSet
 );
 
-// ok
 When(
   /^I press the "([^"]*)?" key$/,
   windowKeyPress
 );
 
-// ok
 When(
   /^I close the last opened (?:window|tab)$/,
   windowLastClose
 );
 
-// ok
 When(
   /^I focus on the last opened (?:window|tab)$/,
   windowLastFocus
 );
 
-// ok
 When(
   /^I (accept|dismiss) the (?:alert|confirm box|prompt)$/,
   windowModalHandle
 );
 
-// ok
 When(
   /^I type "([^"]*)?" on the prompt$/,
   windowModalTextSet
 );
 
-// ok
 When(
   /^I open the url (?:"([^"]*)?"|of the page "([^"]*)?") on a new window$/,
   windowOpen
 );
 
-// ok
 When(
   /^I navigate (back|forward) from the current page(?: (\d+) times)?$/,
   windowPageNavigate
@@ -177,25 +154,21 @@ When(
   windowPause
 );
 
-// ok
 When(
   /^I focus on the parent (?:window|tab)$/,
   windowParentFocus
 );
 
-// ok
 When(
   /^I refresh the page$/,
   windowRefresh
 );
 
-// ok
 When(
   /^I scroll to the (top|bottom) of the page$/,
   windowScrollTo
 );
 
-// ok
 When(
   /^I scroll to the coordinates (\d+).(\d+) of the page$/,
   windowScrollToCoordinates
@@ -207,19 +180,16 @@ When(
   windowSizeMaximize
 );
 
-// ok
 When(
   /^I have a screen that is ([\d]+) by ([\d]+) pixels$/,
   windowSizeSet
 );
 
-// ok
 When(
   /^I focus on the (?:iframe "([^"]*)?"|parent context)$/,
   windowSwitchFrame
 );
 
-// ok
 When(
   /^I open the url (?:"([^"]*)?"|of the page "([^"]*)?")$/,
   windowUrlSet

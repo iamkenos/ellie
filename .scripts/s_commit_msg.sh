578c3f9d1fa51ddd06f8e54423abd87ab3db1a0c
iamkenos <alexis.matunog@gmail.com>
2020-02-10 13:33:44 +0800

    chore: add commit message hook

---
+++ .scripts/s_commit_msg.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+REPO_DIR=$(git rev-parse --show-toplevel)
+RED=$'\e[1;31m'
+RESET=$'\e[0m'
+FORMAT="^(chore|deps|docs|feat|fix|perf|refactor|style|test):[[:space:]].+$"
+MESSAGE=$(cat $REPO_DIR/$HUSKY_GIT_PARAMS)
+
+if [[ ! $MESSAGE =~ $FORMAT ]];
+  then
+    echo "${RED}The commmit message should follow the format:"
+    echo "[type]: description"
+    echo ""
+    echo "[type]:"
+    echo " - chore    : changes to auxillary tools"
+    echo " - deps     : dependency updates"
+    echo " - docs     : documentation only changes"
+    echo " - feat     : a new feature"
+    echo " - fix      : a bug fix"
+    echo " - perf     : code changes that improves performance"
+    echo " - refactor : code changes that neither introduces a new feature or fixes a bug"
+    echo " - style    : code changes that do not affect the behavior of the application"
+    echo " - test     : code changes that deals with tests${RESET}"
+    exit 1
+fi

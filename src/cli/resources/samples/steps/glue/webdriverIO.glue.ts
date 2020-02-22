9c6db56c5bad9910ec1138a36dc915c3fd06050e
iamkenos <alexis.matunog@gmail.com>
2020-02-22 19:14:34 +0800

    fix: reverse statements on glue sample

--- src/cli/resources/samples/steps/glue/webdriverIO.glue.ts
+++ src/cli/resources/samples/steps/glue/webdriverIO.glue.ts
@@ -7,26 +7,26 @@ export function navigate(): void {
   wdioPage.navigate();
 }
 
-export function checkTitle(reverse: boolean): void {
-  wdioPage.checkTitle(reverse);
+export function checkTitle(preferred: boolean): void {
+  wdioPage.checkTitle(!preferred);
 }
 
 export function clickGetStarted(): void {
   wdioPage.getStarted().click();
 }
 
-export function checkProjectTitleText(reverse: boolean, value: string): void {
+export function checkProjectTitleText(preferred: boolean, value: string): void {
   // you can create anonymous functions that encloses a single function which
   // returns a truthy value. this is useful when you want to create custom assertions
   // and still make use of the framework's internal retry mechanism
-  const isProjectTitleTextEquals = (): boolean => wdioPage.projectTitle().isTextEquals(value, reverse);
+  const isProjectTitleTextEquals = (): boolean => wdioPage.projectTitle().isTextEquals(value, !preferred);
   driver.checkCustomTruthy(isProjectTitleTextEquals);
 
   // the check above is for illustration purposes and can be simplified by
   // using the statement below
-  // wdioPage.projectTitle().checkTextEquals(value, reverse);
+  // wdioPage.projectTitle().checkTextEquals(value, !preferred);
 }
 
-export function checkNavBarDisplayed(reverse: boolean): void {
-  wdioPage.navBar().checkDisplayed(reverse);
+export function checkNavBarDisplayed(preferred: boolean): void {
+  wdioPage.navBar().checkDisplayed(!preferred);
 }

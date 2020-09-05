c510124fbbafeb1ccf95e44a1120d439980fec49
iamkenos <alexis.matunog@gmail.com>
2020-09-05 19:34:16 +0800

    feat: show allure report open command on test finish

--- src/core/run.ts
+++ src/core/run.ts
@@ -4,10 +4,13 @@ import logger from "../logger";
 
 export default (wdioConfFile: string): void => {
   const wdio = new Launcher(wdioConfFile, {}, false);
+  const config: any = wdio.configParser.getConfig();
   logger.info("Starting WebdriverIO runner...");
   wdio.run().then(
     (code: number): void => {
       logger.info("Finished");
+      console.log("To launch the HTML report:");
+      console.log(`$ npx allure open '${config.allureHtmlDir}'`);
       process.exit(code);
     },
     (error: any): void => {
--- src/types/@wdio/cli/index.d.ts
+++ src/types/@wdio/cli/index.d.ts
@@ -4,6 +4,8 @@ declare module "@wdio/cli" {
   export default class Launcher {
     constructor(configFile: string, argv: any, isWatchMode: boolean);
 
+    public configParser: { getConfig(): WebdriverIO.Config }
+
     run(): Promise<any>;
   }
 }
